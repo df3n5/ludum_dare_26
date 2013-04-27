708fccca34396622da73649254ef44b1c5f574e3
Jonathan Frawley <jonathanfrawley@gmail.com>
2013-04-27 17:09:05 +0100

    Added puzzle level with shrinking space.

--- src/ludum_dare_26/main.lua
+++ src/ludum_dare_26/main.lua
@@ -15,7 +15,37 @@ Title = Tile:extend {
     image = 'media/title.png',
 }
 
-Player = Fill:extend {
+resetNeeded = false
+
+function saveOrigPos(obj)
+    obj.origX = obj.x
+    obj.origY = obj.y
+    obj.origVel = obj.velocity
+    obj.origAcceleration = obj.acceleration
+end
+
+function reset(obj)
+    obj.x = obj.origX
+    obj.y = obj.origY
+    obj.velocity = obj.origVel
+    obj.acceleration = obj.origAcceleration
+end
+
+ResetableFill = Fill:extend {
+    onNew = function(self)
+        saveOrigPos(self)
+    end,
+    reset = reset
+}
+
+ResetableTile = Tile:extend {
+    onNew = function(self)
+        saveOrigPos(self)
+    end,
+    reset = reset
+}
+
+Player = ResetableFill:extend {
     x = 0, 
     y = 0, 
     width = 16, 
@@ -25,9 +55,11 @@ Player = Fill:extend {
     moveMode = "normal",
     --moveMode = "onlyJump",
     --conveyorMode = "conveyorLeft",
+    --moveMode = "topdown",
     conveyorMode = "normal",
     canFire = true,
     direction = 'right',
+    speed = 200,
 
     onUpdate = function (self)
         self.velocity.x = 0
@@ -37,17 +69,27 @@ Player = Fill:extend {
         if self.conveyorMode == "conveyorRight" and (self.canJump) then
             self.velocity.x = 100
         end
-        if self.moveMode=="normal" or (self.moveMode=="onlyJump" and not self.canJump) then
+        if self.moveMode=="topdown" or self.moveMode=="normal" or (self.moveMode=="onlyJump" and not self.canJump) then
             if the.keys:pressed('left') then
-                self.velocity.x = self.velocity.x - 150
+                self.velocity.x = self.velocity.x - self.speed
                 self.direction = 'left'
             elseif the.keys:pressed('right') then
-                self.velocity.x = self.velocity.x + 150
+                self.velocity.x = self.velocity.x + self.speed
                 self.direction = 'right'
             else
                 if self.conveyorMode == "none" then
                     self.velocity.x = 0
                 end
+            end
+        end
+
+        if self.moveMode == "topdown" then
+            if the.keys:pressed('up') then
+                self.velocity.y = -self.speed
+            elseif the.keys:pressed('down') then
+                self.velocity.y = self.speed
+            else
+                self.velocity.y = 0
             end
         end
 
@@ -62,7 +104,7 @@ Player = Fill:extend {
 
         -- Reset if you go off screen
         if self.y > the.app.height then
-            self:reset()
+            resetNeeded = true
         end
         self.lastpos = {x=self.x, y=self.y}
     end,
@@ -74,21 +116,37 @@ Player = Fill:extend {
             self.canJump = true
         end
     end,
-    reset = function(self)
-        self.x = 0
-        self.y = 0
-    end
 }
 
-Platform = Fill:extend {
+Platform = ResetableFill:extend {
     x = 0, 
     y = 0, 
     width = 128,
     height = 32,
     fill = {0, 0, 0},
     platformType = "normal",
+    squashMode = "vertical",
     --platformType = "breakable",
     isDying = false,
+    jumpable = false,
+    movementToCentreSpeed = 80,
+
+    moveTowardsCentre = function(self)
+        if self.squashMode=="vertical" then
+            if(self.y > the.app.height/2) then
+                self.velocity.y = -self.movementToCentreSpeed
+            else
+                self.velocity.y = self.movementToCentreSpeed
+            end
+        elseif self.squashMode=="horizontal" then
+            if(self.x > the.app.width/2) then
+                self.velocity.x = -self.movementToCentreSpeed
+            else
+                self.velocity.x = self.movementToCentreSpeed
+            end
+        end
+    end,
+
     onUpdate = function (self, elapsed)
         if self.isDying then
             self.timeToDie = self.timeToDie - elapsed
@@ -97,8 +155,14 @@ Platform = Fill:extend {
             end
         end
     end,
+
     onCollide = function (self, other)
-        if(other.velocity.y > 0) then
+        if other:instanceOf(Platform) then
+            print("2 platforms colliding")
+            self:die()
+            other:die()
+        end
+        if (other.velocity.y > 0) or (not self.jumpable) then
             if not self.isDying then
                 if self.platformType == "breakable" then
                     self.timeToDie = 0.5
@@ -110,18 +174,19 @@ Platform = Fill:extend {
     end
 }
 
-Spike = Tile:extend {
+Spike = ResetableTile:extend {
     x = 0, 
     y = 0, 
     width = 32,
     height = 32,
     image = "media/spike.png",
+
     onCollide = function (self, other)
-        other:reset()
+        resetNeeded = true
     end
 }
 
-Boss = Tile:extend {
+Boss = ResetableTile:extend {
     x = 0, 
     y = 0, 
     width = 128,
@@ -132,7 +197,7 @@ Boss = Tile:extend {
     
     onCollide = function (self, other)
         if other:instanceOf(Player) then
-            other:reset()
+            resetNeeded = true
         end
     end,
 
@@ -145,7 +210,7 @@ Boss = Tile:extend {
     end
 }
 
-Bullet = Fill:extend {
+Bullet = ResetableFill:extend {
     x = 0, 
     y = 0, 
     width = 8,
@@ -160,6 +225,14 @@ Bullet = Fill:extend {
     end
 }
 
+Door = ResetableFill:extend {
+    x = 0, 
+    y = 0, 
+    width = 32,
+    height = 48,
+    fill = {0, 255, 12},
+    border = {0, 0, 0}
+}
 
 the.app = App:new {
     loadLevel = function(self, level) 
@@ -205,6 +278,38 @@ the.app = App:new {
             self:add(self.boss) 
             self.bullets = Group:new()
             self:add(self.bullets)
+        elseif level == 3 then
+            self.platforms = Group:new()
+            self.platforms:add(Platform:new{x=100,y=100,width=600})
+            self.platforms:add(Platform:new{x=200,y=200,width=600})
+            self.platforms:add(Platform:new{x=150,y=300,width=550})
+            self.platforms:add(Platform:new{x=400,y=450,width=400})
+            self:add(self.platforms)
+            self.door = Door:new{x=400,y=450-Door.height}
+            self:add(self.door)
+
+            self.sideSpikes = Group:new()
+            n_spikes = (600 / 32)+1
+            print(n_spikes)
+            for i=1, n_spikes do
+                spike = Spike:new{x=0, y=(i-1)*Spike.height, rotation=math.rad(-90)}
+                spike.velocity.x = 10
+                self.sideSpikes:add(spike)
+            end
+            self:add(self.sideSpikes)
+
+            self.player = Player:new{x=100, y=0}
+            self:add(self.player) 
+        elseif level == 4 then
+            self.platforms = Group:new()
+            self.platforms:add(Platform:new{x=100,y=100,width=700, height=16, squashMode="vertical"})
+            self.platforms:add(Platform:new{x=100,y=500,width=700, height=16, squashMode="vertical"})
+            self.platforms:add(Platform:new{x=-200,y=100,width=100, width=16, height = the.app.height-100, squashMode="horizontal"})
+            self.platforms:add(Platform:new{x=the.app.width+200,y=100,width=16, height = the.app.height-100, squashMode="horizontal"})
+            self:add(self.platforms)
+
+            self.player = Player:new{x=the.app.width/2, y=the.app.height/2, acceleration={x=0, y=0}, moveMode="topdown" }
+            self:add(self.player) 
         end
     end,
     onRun = function (self)
@@ -225,7 +330,9 @@ the.app = App:new {
                     self:remove(self.title)
                     self.state = "level"
                     --self.level = 1
-                    self.level = 2
+                    --self.level = 2
+                    --self.level = 3
+                    self.level = 4
                     self:loadLevel(self.level)
                 end
             end
@@ -248,6 +355,45 @@ the.app = App:new {
                 if the.keys:justPressed('a') and self.player.canFire then
                     self:fire(self.player)
                 end               
+            elseif self.level == 3 then
+                self.platforms:collide(self.player)
+                self.sideSpikes:collide(self.player)
+                if(resetNeeded == true) then
+                    reset(self.player)
+                    for k, spike in pairs(self.sideSpikes.sprites) do
+                        reset(spike)
+                    end
+                    resetNeeded = false
+                end
+                x, y = self.player:overlap(self.door.x, self.door.y, self.door.width, self.door.height)
+                if x>0 or y>0 then
+                    if the.keys:justPressed('a') then
+                        playSound('media/levelFinish.wav')
+                    end
+                end
+            elseif self.level == 4 then
+                --check to see if sqashed by 2 platforms
+                nCollisions = 0
+                for k, platform in pairs(self.platforms.sprites) do
+                    if platform.active then
+                        x, y = self.player:overlap(platform.x, platform.y, platform.width, platform.height)
+                        if x>0 or y>0 then
+                            nCollisions = nCollisions + 1
+                        end
+                    end
+                end
+                if nCollisions > 1 then
+                    resetNeeded = true
+                end
+                self.platforms:collide(self.player)
+                self.platforms:collide(self.platforms)
+                for k, platform in pairs(self.platforms.sprites) do
+                    platform:moveTowardsCentre()
+                end
+                if(resetNeeded == true) then
+                    self:loadLevel(self.level)
+                    resetNeeded = false
+                end
             end
         end
     end,
